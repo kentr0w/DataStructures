8cb6f6159b9ce55a7601cf179428ea093248092e
kentr0w <38314854+kentr0w@users.noreply.github.com>
2019-06-05 02:05:42 +0300

    Add files via upload

---
+++ B-tree/src/cmake-build-debug/CMakeFiles/btree_main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/btree_main.dir/main.cpp.o"
+  "CMakeFiles/btree_main.dir/btree.cpp.o"
+  "btree_main.pdb"
+  "btree_main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/btree_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
